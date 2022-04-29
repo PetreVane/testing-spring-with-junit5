86df555d75cbac05e689cdb32d01a5b6baed948d
Petre Vane <petre.vane@gmail.com>
2022-04-29 17:29:42 +0200

    Changes: - added models

---
+++ src/main/java/com/luv2code/springboot/SpringBootTestingExample.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestingExample.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/luv2code/springboot/models/Student.java
@@ -0,0 +1,8 @@
+package com.luv2code.springboot.models;
+
+public interface Student {
+
+    String studentInformation();
+
+    String getFullName();
+}
