7e86f42c2fbe4f91974a3153bf468dfec7a2a5dc
Petre Vane <petre.vane@gmail.com>
2022-04-29 17:35:04 +0200

    Changes: - properties and a new bean

--- src/main/java/com/luv2code/springboot/SpringBootTestingExample.java
+++ src/main/java/com/luv2code/springboot/SpringBootTestingExample.java
@@ -1,13 +1,23 @@
 package com.luv2code.springboot;
 
+import com.luv2code.springboot.models.CollegeStudent;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class SpringBootTestingExample {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTestingExample.class, args);
+    }
+
+    @Bean
+    @Scope(value = "prototype v")
+    CollegeStudent collegeStudent() {
+        return new CollegeStudent();
     }
 
 }
